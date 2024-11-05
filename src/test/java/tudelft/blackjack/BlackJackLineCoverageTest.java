89e8cde896015eb06c9089398c3db92f3d76ce5c
khasssabi <khasssabi@gmail.com>
2024-11-05 15:14:19 +0300

    Add parameterized tests for BlackJack scoring logic

--- src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
+++ src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
@@ -2,6 +2,8 @@ package tudelft.blackjack;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class BlackJackLineCoverageTest {
 
@@ -15,5 +17,23 @@ public class BlackJackLineCoverageTest {
     public void leftPlayerWins() {
         int result = new BlackJack().play(10, 9);
         Assertions.assertEquals(10, result);
+    }
+
+    @ParameterizedTest(name="left={0}, right={1},wins={2}")
+    @CsvSource({
+            "21, 20, 21",           // left wins
+            "21, 21, 21",           // both 21 -- both win
+            "19, 20, 20",           // right wins
+            "0, 0, 0",              // 0
+            "20, 23, 20",           // right exceeds the limit, left wins
+            "22, 0, 0",             // left exceeds the limit, right is 0, 0
+            "-1, 1, 1",             // left negative - right wins
+            "-1, -2, -1",           // both negative, left wins
+            "23, 22, 0",            // both exceed the limit, left is bigger, 0
+            "100, 100, 0"           // both exceed the limit, 0
+    })
+    public void allCombinations(int left, int right, int wins) {
+        int result = new BlackJack().play(left, right);
+        Assertions.assertEquals(wins, result);
     }
 }
