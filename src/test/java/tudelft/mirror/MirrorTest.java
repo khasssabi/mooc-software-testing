e8c8104494a92d45a3b959aec30c6f630295c8df
khasssabi <khasssabi@gmail.com>
2024-11-08 18:25:29 +0300

    Fix off-by-one error in the loop of mirrorEnds method and add tests

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,88 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//import sun.tools.jconsole.JConsole;
+
 public class MirrorTest {
+    @Test
+    public void oneCharacter() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("xscx");
+        Assertions.assertEquals("x", result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void twoCharacters() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("baobab");
+        Assertions.assertEquals("ba", result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void threeCharacters() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("12302321");
+        Assertions.assertEquals("123", result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void zeroCharactersMirror() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("abcd");
+        Assertions.assertEquals("", result);
+        System.out.println(result + "zero characters mirror");
+    }
+
+    @Test
+    public void emptyString() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("");
+        Assertions.assertEquals("", result);
+        System.out.println(result + "empty string");
+    }
+
+    @Test
+    public void oneLetter() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("a");
+        Assertions.assertEquals("a", result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void twoSameLetters() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("ww");
+        Assertions.assertEquals("w", result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void allMirrorEven() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("deed");
+        Assertions.assertEquals("de", result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void allMirrorUneven() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("deded");
+        Assertions.assertEquals("ded", result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void palindrome() {
+        Mirror mr = new Mirror();
+        String result = mr.mirrorEnds("nursesrun");
+        Assertions.assertEquals("nurse", result);
+        System.out.println(result);
+    }
+
 }
