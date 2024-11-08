715e7577c83b4a764c66ef469de53282558fc191
khasssabi <khasssabi@gmail.com>
2024-11-08 18:16:36 +0300

    Add new test invoices

--- src/test/java/tudelft/invoicemocked/InvoiceFilterTest.java
+++ src/test/java/tudelft/invoicemocked/InvoiceFilterTest.java
@@ -13,17 +13,21 @@ public class InvoiceFilterTest {
 
         Invoice mauricio = new Invoice("Mauricio", 20.0);
         Invoice arie = new Invoice("Arie", 300.0);
+        Invoice alice = new Invoice("Alice", 55.0);
+        Invoice  mark = new Invoice("Mark", 115.0);
 
         InvoiceDao dao = Mockito.mock(InvoiceDao.class);
 
-        List<Invoice> results = Arrays.asList(mauricio, arie);
+        List<Invoice> results = Arrays.asList(mauricio, arie, alice, mark);
         Mockito.when(dao.all()).thenReturn(results);
 
         InvoiceFilter filter = new InvoiceFilter(dao);
         List<Invoice> result = filter.filter();
 
         Assertions.assertEquals(mauricio, result.get(0));
-        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals(2, result.size());
+        Assertions.assertEquals(alice, result.get(1));
+
     }
 
 }
